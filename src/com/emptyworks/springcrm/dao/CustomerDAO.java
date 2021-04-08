ae1213dab93a9194ef67a0a4c9239cd2318b2b89
empty-works <pama.mark@gmail.com>
2021-04-08 19:54:02 +0200

    DAO package and created customer DAO.

---
+++ src/com/emptyworks/springcrm/dao/CustomerDAO.java
@@ -0,0 +1,12 @@
+package com.emptyworks.springcrm.dao;
+
+import java.util.List;
+
+import com.emptyworks.springcrm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+	
+}
