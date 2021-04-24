f59ca38cda1476f4c9af6a556443e85a26694c04
empty-works <pama.mark@gmail.com>
2021-04-24 20:01:00 +0200

    Finished adding AOP logging support.

--- src/com/emptyworks/springcrm/aspect/CRMLoggingAspect.java
+++ src/com/emptyworks/springcrm/aspect/CRMLoggingAspect.java
@@ -2,7 +2,10 @@ package com.emptyworks.springcrm.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,19 +17,48 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// Set up pointcut declaration
-	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.emptyworks.springcrm.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.luv2code.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.emptyworks.springcrm.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.emptyworks.springcrm.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
 	private void forAppFlow() {}
 	
 	// Add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// Display the method being called
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======>> in @Before: calling method: " + theMethod);
+		
+		// Display the arguments to the method
+		
+		// Get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		
+		// Loop through and display args
+		for(Object tempArg : args) {
+			myLogger.info("=======>> argument: " + tempArg);
+		}
+	}
 	
 	// Add @AfterReturning advice
+	@AfterReturning (
+			pointcut="forAppFlow()", 
+			returning="theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// Display method being returned from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======>> in @AfterReturning: calling method: " + theMethod);
+		
+		// Display data returned 
+		myLogger.info("=======>> result: " + theResult);
+	}
 }
